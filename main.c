7bde8146f93479b21a7168b2c27f614131d2b63d
kriggyrd <43894260+kryggird@users.noreply.github.com>
2024-06-26 14:36:01 +0300

    Handle received packets

--- main.c
+++ main.c
@@ -14,15 +14,19 @@
 const size_t BUFFER_SIZE = 4096;
 const size_t NUM_BUFFERS = 1024;
 
+const int IO_QUEUE_DEPTH = 64;
+const int NUM_CQE_SLOTS = IO_QUEUE_DEPTH * 16;
+
 void run_server() {
     int fd = init_socket_v6(8080);
     
     buffer_pool_t pool = init_buffer_pool(BUFFER_SIZE, NUM_BUFFERS);
+    struct io_uring_cqe* cqe_slots[NUM_CQE_SLOTS];
 
     struct io_uring ring;
-    init_ring(&ring);
+    init_ring(&ring, IO_QUEUE_DEPTH);
     register_buffer_pool(&ring, &pool);
-    io_uring_register_files(&ring, &fd, 1);
+    io_uring_register_files(&ring, &fd, 1); // TODO get registered file idx
 
     struct msghdr msg = (struct msghdr){
 	.msg_namelen = sizeof(struct sockaddr_storage), // TODO Why not 0?
@@ -31,6 +35,34 @@ void run_server() {
     prep_recv_multishot(&ring, &msg);
 
     io_uring_submit_and_wait(&ring, 1);
+    
+    for (;;) {
+	int ret = io_uring_submit_and_wait(&ring, 1);
+	if (ret == -EINTR) { continue; }
+	if (ret < 0) { break; }
+
+	size_t new_cqe_count = io_uring_peek_batch_cqe(&ring, cqe_slots, NUM_CQE_SLOTS);
+
+	for (size_t cqe_idx = 0; cqe_idx < new_cqe_count; ++cqe_idx) {
+	    // TODO: Handle IORING_CQE_F_MORE and IORING_CQE_F_BUFFER
+	    
+	    op_metadata_t op_meta = { .as_u64 = cqe_slots[cqe_idx]->user_data };
+	
+	    if (op_meta.is_recvmsg) {
+		recvmsg_result_t res = validate_recvmsg(cqe_slots[cqe_idx], &pool, &msg);
+
+		if (res.is_valid) {
+		    prep_sendmsg(&ring, NULL, &res);
+		}
+
+	    } else {
+		add_buffer(&pool, op_meta.buffer_idx);
+		io_uring_buf_ring_advance(pool.metadata, 1);
+	    }
+
+	    io_uring_cq_advance(&ring, new_cqe_count);
+	}
+    }
 }
 
 int main() {
--- ring_helper.h
+++ ring_helper.h
@@ -2,19 +2,23 @@
 
 #include "liburing.h"
 
-const int IO_QUEUE_DEPTH = 64;
-
-void init_ring(struct io_uring* ring) {
+void init_ring(struct io_uring* ring, unsigned int queue_depth) {
     // TODO error checking
-    int ret = io_uring_queue_init_params(IO_QUEUE_DEPTH, ring, &(struct io_uring_params) {
-	.cq_entries = IO_QUEUE_DEPTH,
+    int ret = io_uring_queue_init_params(queue_depth, ring, &(struct io_uring_params) {
+	.cq_entries = queue_depth * 8u,
 	.flags = IORING_SETUP_SUBMIT_ALL 
 	| IORING_SETUP_COOP_TASKRUN 
 	| IORING_SETUP_CQSIZE    
     });
-
-
 }
+
+typedef union op_metadata_t {
+    uint64_t as_u64;
+    struct {
+	uint32_t is_recvmsg;
+	uint32_t buffer_idx;
+    };
+} op_metadata_t;
 
 typedef struct {
     size_t num_buffers;
@@ -34,6 +38,18 @@ buffer_pool_t init_buffer_pool(size_t buf_size, size_t num_buffers) {
     return (buffer_pool_t){num_buffers, buf_size, mmap_size, metadata, buffers};
 }
 
+unsigned char* buffer_pointer(buffer_pool_t* pool, size_t idx) {
+    return pool->buffers + idx * pool->buf_size;
+}
+
+void add_buffer(buffer_pool_t* pool, size_t idx) {
+    io_uring_buf_ring_add(pool->metadata, 
+			  buffer_pointer(pool, idx),
+			  pool->buf_size, idx,
+			  io_uring_buf_ring_mask(pool->num_buffers), 
+			  idx);
+}
+
 void register_buffer_pool(struct io_uring* ring, buffer_pool_t* pool) {
     io_uring_buf_ring_init(pool->metadata);
 
@@ -45,14 +61,17 @@ void register_buffer_pool(struct io_uring* ring, buffer_pool_t* pool) {
 
     int ret = io_uring_register_buf_ring(ring, &reg, 0);
     for (size_t idx = 0; idx < pool->num_buffers; ++idx) {
-	io_uring_buf_ring_add(pool->metadata, 
-			      pool->buffers + pool->buf_size * idx,
-			      pool->buf_size, idx,
-			      io_uring_buf_ring_mask(pool->num_buffers), 
-			      idx);
+	// io_uring_buf_ring_add(pool->metadata, 
+	// 		      buffer_pointer(pool, idx),
+	// 		      pool->buf_size, idx,
+	// 		      io_uring_buf_ring_mask(pool->num_buffers), 
+	// 		      idx);
+	add_buffer(pool, idx);
     }
     io_uring_buf_ring_advance(pool->metadata, pool->num_buffers);
 }
+
+
 
 int prep_recv_multishot(struct io_uring* ring, struct msghdr* msg) {
 	struct io_uring_sqe* sqe = io_uring_get_sqe(ring);
@@ -63,7 +82,70 @@ int prep_recv_multishot(struct io_uring* ring, struct msghdr* msg) {
 	sqe->flags |= IOSQE_FIXED_FILE | IOSQE_BUFFER_SELECT;
 	sqe->buf_group = 0;
 
-	io_uring_sqe_set_data64(sqe, 0);
+	op_metadata_t meta = {.buffer_idx = 0, .is_recvmsg = 1};
+	io_uring_sqe_set_data64(sqe, meta.as_u64);
 	return 0;
+}
+
+typedef struct recvmsg_result_t {
+    int is_valid;
+    size_t buffer_idx;
+    void* msg_name;
+    uint32_t msg_name_length;
+    void* msg_payload;
+    unsigned int msg_payload_length;
+} recvmsg_result_t;
+
+recvmsg_result_t validate_recvmsg(struct io_uring_cqe* cqe, buffer_pool_t* pool, struct msghdr* msg) {
+    size_t idx = ((size_t) cqe->flags) >> 16;
+
+    struct io_uring_recvmsg_out* slot = io_uring_recvmsg_validate(buffer_pointer(pool, idx), cqe->res, msg);
+
+    if (!slot || slot->namelen > msg->msg_namelen || slot->flags & MSG_TRUNC) {
+	// recycle_buffer(ctx, idx);
+	return (recvmsg_result_t){0};
+    }
+
+    return (recvmsg_result_t){
+	.is_valid = 1, 
+	.buffer_idx = idx,
+	.msg_name = io_uring_recvmsg_name(slot),
+	.msg_name_length = slot->namelen,
+	.msg_payload = io_uring_recvmsg_payload(slot, msg),
+	.msg_payload_length = io_uring_recvmsg_payload_length(slot, cqe->res, msg)
+    };
+}
+
+typedef struct sendmsg_metadata_t {
+    struct iovec iov;
+    struct msghdr msghdr;
+} sendmsg_metadata_t;
+
+// TODO bounds check
+int prep_sendmsg(struct io_uring* ring, sendmsg_metadata_t metadata_array[], recvmsg_result_t* res) {
+    // TODO: flush submit ring if one cannot get an SQE?
+    struct io_uring_sqe* sqe = io_uring_get_sqe(ring);
+
+    sendmsg_metadata_t* meta = &metadata_array[res->buffer_idx];
+    meta->iov = (struct iovec) {
+	.iov_base = res->msg_payload,
+	.iov_len = res->msg_payload_length,
+    };
+    meta->msghdr = (struct msghdr) {
+	.msg_namelen = res->msg_name_length,
+	.msg_name = res->msg_name,
+	.msg_control = NULL,
+	.msg_controllen = 0,
+	.msg_iov = &(meta->iov),
+	.msg_iovlen = 1
+    };
+
+    op_metadata_t op_meta = {.buffer_idx = (uint32_t) res->buffer_idx, .is_recvmsg = 0};
+
+    io_uring_prep_sendmsg(sqe, 0 /* registered socket idx */, &(meta->msghdr), 0);
+    io_uring_sqe_set_data64(sqe, op_meta.as_u64);
+    sqe->flags |= IOSQE_FIXED_FILE; // TODO fold into flags argument of prep_sendmsg?
+
+    return 0;
 }
 
